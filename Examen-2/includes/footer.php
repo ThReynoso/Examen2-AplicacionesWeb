ad76db36c912cdf25dfb54d85c93897c04efe969
ThReynosoDxD <gaelomreynoso@gmail.com>
2024-10-31 19:35:16 -0700

    Add files via upload

---
+++ Examen-2/includes/footer.php
@@ -0,0 +1,7 @@
+<section>
+        <footer>
+            <div>
+                <p>TODOS LOS DERECHOS RESERVADOS BY I</p>
+            </div>
+        </footer>
+    </section>
---
+++ Examen-2/includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bines Raices</title>
+</head>
+<body>
+
+    <section><h1>Bines Raices Ray</h1>
+        <div>
+            <nav>
+                <a href="index.php">Inicio</a>
+                <a href="Nosotros.php">Nosotros</a>
+                <a href="crearSellers.php">Crear un Vendedor</a>
+                <a href="crearPropiedades.php">Crear una propiedad</a>
+                <a href="VentasAgente.php">Historial de ventas</a>
+                <a href="Blog.php">Blog</a>
+                <a href="Contactanos.php">Contactanos</a>
+            </nav>
+        </div>
+    </section>
